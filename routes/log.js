d9a04b0fa66de4df13e68cf3e332ccbb50d959cb
Samantha Guanzon <45184463+seguanzon@users.noreply.github.com>
2021-11-28 13:33:14 -0800

    Restored log.js with Bao's old version
    (need to update with what he pushed last night)

--- routes/log.js
+++ routes/log.js
@@ -1,127 +1,57 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <link href="/css/style.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <meta charset="UTF-8">
-  <title>Life Log</title>
-  <link href="/css/style.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const User = require('../models/user')
 
-<body style="background-color:#20201F">
-  <!-- <br><br><img src = "/images/LeafLogo.png" width = 180%></src> -->
-  
-  <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="https://code.highcharts.com/modules/xrange.js"></script>
-  <script src="https://code.highcharts.com/modules/exporting.js"></script>
-  <script src="https://code.highcharts.com/modules/draggable-points.js"></script>
-  <script src="https://code.highcharts.com/highcharts.js"></script>
-  <script src="https://code.highcharts.com/modules/draggable-points.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-    crossorigin="anonymous"></script>
-  
-  <script src="js/jquery-3.6.0.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
+router.get('/', (req, res) => {
+   console.log("Loading User page");
+   res.render('log.ejs', {sessionData: req.session});
+   // res.render('log.ejs', {userSessionData: req.session.user.userData});
+   // id = req.session.user_id;
 
-  <script type="text/javascript">
-    $(document).ready(function () {
-      $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-      });
-    });
-  </script>
+   let newEntry = {
+      x: Date.UTC(2021, 9, 5, 7),
+      x2: Date.UTC(2021, 9, 5, 9),
+      activityName: 'testing',
+      color: '#FFFFFF',
+      comments: 'DID PUSH?',
+      y: 0
+   };
+   req.session.user.userData.push(newEntry);
 
-  <div class="wrapper">
-    <!-- Sidebar Holder -->
-    <nav id="sidebar">
-      <div class="sidebar-header">
-        <h1>Life Log</h1>
-      </div>
-      <ul class="components">
-        <li>
-          <a href="log" style="background-color: #351c75ff;">
-            <h2>
-              Log
-            </h2>
-          </a>
-        </li>
-        <li>
-          <a href="#statSubmenu" data-toggle="collapse" aria-expanded="false">
-            <h2>
-              Statistics
-            </h2>
-          </a>
-          <ul class="collapse list-unstyled" id="statSubmenu">
-            <li><a href="yearly"><h3>Yearly Summary</h3></a></li>
-            <li><a href="weekly"><h3>Weekly Summary</h3></a></li>
-            <li><a href="goals"><h3>Goals</h3></a></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-    
-    <div id="content">
-      <img id = "Left" src = "/images/LeftButton.png"></src>
-      <img id = "Right" src = "/images/RightButton.png"></src>
-      <figure class="highcharts-figure">
-        <div id="container1"></div>
-        <style>
-          .highcharts-container svg {
-            overflow: visible !important;
-          }
-          .highcharts-container {
-            overflow: visible !important;
-          }
-        </style>
-        <script src="logChart.js"></script>
-        <script>
-          var userSessionData = <%-JSON.stringify(userData) %>;
-          displayLogChart(userSessionData);
-        </script>
-      </figure>
-    </div>
+});
 
-    <div id = "div3">
-      <div id="TotalHours"></div>
-      <div id="container2"></div>
-      <script>
-        var userSessionData = <%-JSON.stringify(userData) %>;
-        var activities = <%-JSON.stringify(userActivities) %>;
-        displayPieChart(userSessionData, activities)
-      </script>
+router.post('/log', (req, res) => {
+   console.log("posting log");
+   
+   let newUserData = req.session.user.userData;
+   console.log("new user data " + newUserData);
+   console.log("new user data x " + newUserData);
 
-    </div>
+   let newEntry = {
+      x: req.body.x,
+      x2: req.body.x2,
+      activityName: req.body.activityName,
+      color: req.body.color,
+      comments: req.body.comments,
+      y: 0
+   };
 
+   newUserData.push(newEntry);
+   // newUserData.x.push(req.body.x);
+   // newUserData.x2.push(req.body.x2);
+   // newUserData.activityName.push(req.body.activityName);
+   // newUserData.color.push(req.body.color);
+   // newUserData.comments.push(req.body.comments);
 
+   User.findByIdAndUpdate({_id: req.session.user._id}, {userData: newUserData}, {useFindAndModify:true}, function(err, res) {
+       if (err)
+           console.log('err, ID not found', err);
+       else
+           console.log('successfully updated id');
+   });
+   userSession = newUserData;
+   res.redirect('/log'); 
+});
 
-    
-    <div id = "div4">
-      <form action="/log/createActivity" method="POST">
-        <input type="color" placeholder="Color" name="color" required><br>
-        <input id = 'dropdown' type="text" placeholder="Activity Name" name="activityName" required><br>
-        <button id = 'button' type="submit">Create <br> Activity</button>
-        <br><br><br><br>
-      </form>
-
-      <form action="/log/logActivity" method="POST">
-        <select id = 'dropdown' name="userActivities" id="cars">
-          <% for(var i = 0; i < userActivities.length; i++) { %>
-            <option><%= userActivities[i].activityName %></option>  
-          <% }; %>
-        </select> <br>
-        <input id = 'dropdown' type="datetime-local" placeholder="Start Time" name="x" required><br>
-        <input id = 'dropdown' type="datetime-local" placeholder="End Time" name="x2" required><br>
-        <input id = 'dropdown' type="text" placeholder="Comments" name="comments" required><br>
-        <button id = 'button' type="submit">Log <br> Activity</button>
-      </form>
-    </div>
-  </div>
-
-</body>
-
-<div id="dragstatus"></div>
-
-</html>
+module.exports = router;
