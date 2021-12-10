f3dd278fdd370c2322ed5aba08be3976d95e9bce
JonathanBorda3 <41019523+JonathanBorda3@users.noreply.github.com>
2021-12-09 18:18:41 -0800

    Update weekly.js

--- routes/weekly.js
+++ routes/weekly.js
@@ -5,11 +5,15 @@ const User = require('../models/user')
 
 
 router.get('/', (req, res) => {
-    console.log("Loading Monthly page");
-    res.render('weekly.ejs');
+    console.log("Loading Weekly page");
+    res.render('weekly.ejs', {userData: req.session.user.userData, userActivities: req.session.user.userActivities, week: req.session.user.week});
+    console.log(userData);
 });
 
+
+router.post('/UpdateWeek', (req, res) => {
+    req.session.user.week = req.body.week;
+    req.session.save();
+    res.redirect('/weekly');
+});
 module.exports = router;
-
-
-
